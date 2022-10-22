8e9d9db1f6e21ad62852e8e50d05d21e16fa32a0
Maeevick <sayag_vs@hotmail.fr>
2022-10-22 18:02:33 +0200

    tech: responsive header improvement

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,9 @@
 const Header = () => {
     return (
-        <header className="flex w-full flex-col items-center border-b-orange-600 border-b-2 text-center">
-            <p className="text-6xl font-bold "><a href="/">MicroSaaS Maker!</a></p>
-            <nav className="w-full flex justify-center space-x-4 mt-3 mb-3">
-                <a className="p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow" href="/api/hello">A Propos</a>
+        <header className="flex w-full flex-col md:flex-row items-center border-b-orange-600 border-b-2 text-center">
+            <p className="text-5xl font-bold px-3 md:whitespace-nowrap"><a href="/">MicroSaaS Maker!</a></p>
+            <nav className="w-full px-3 flex justify-center space-x-6 my-3">
+                <a className="p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow" href="/">A Propos</a>
                 <a className="p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow" href="https://maeevick.github.io/blog.html" target="_blank">Blog</a>
                 <a className="p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow" href="https://www.youtube.com/channel/UCmCyGWHYIjGBdRdDigT2Erg" target="_blank">Youtube</a>
                 <a className="p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow" href="/products">Produits</a>
