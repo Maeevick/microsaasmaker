1afe38d6df60b32116a748f58425271a98bb9376
Maeevick <sayag_vs@hotmail.fr>
2022-10-22 17:50:43 +0200

    feat: add navigation to blog (temp)

--- components/header.tsx
+++ components/header.tsx
@@ -4,6 +4,7 @@ const Header = () => {
             <p className="text-6xl font-bold "><a href="/">MicroSaaS Maker!</a></p>
             <nav className="w-full flex justify-center space-x-4 mt-3 mb-3">
                 <a className="p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow" href="/api/hello">A Propos</a>
+                <a className="p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow" href="https://maeevick.github.io/blog.html" target="_blank">Blog</a>
                 <a className="p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow" href="https://www.youtube.com/channel/UCmCyGWHYIjGBdRdDigT2Erg" target="_blank">Youtube</a>
                 <a className="p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow" href="/products">Produits</a>
             </nav>
