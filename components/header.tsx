ca9196b492b5e881d79b119babacfd26ef5edaaf
Aurel <sayag_vs@hotmail.fr>
2022-11-16 19:26:37 +0000

    tech: improve Link with next 13

--- components/header.tsx
+++ components/header.tsx
@@ -11,9 +11,9 @@ const HomeTitle = () => {
 const NavBar = () => {
     return (
         <nav className="w-full flex space-x-3 justify-center text-[0.8rem] md:text-base">
-            <Link href={{ pathname: '/[title]', query: { title: 'pourquoi' } }}><a className="my-1 p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow" href="">Pourquoi</a></Link>
-            <Link href={{ pathname: '/[title]', query: { title: 'quoi' } }}><a className="my-1 p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow" href="">Quoi</a></Link>
-            <Link href={{ pathname: '/[title]', query: { title: 'comment' } }}><a className="my-1 p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow" href="">Comment</a></Link>
+            <Link href={{ pathname: '/[title]', query: { title: 'pourquoi' } }} className="my-1 p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow">Pourquoi</Link>
+            <Link href={{ pathname: '/[title]', query: { title: 'quoi' } }} className="my-1 p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow">Quoi</Link>
+            <Link href={{ pathname: '/[title]', query: { title: 'comment' } }} className="my-1 p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow">Comment</Link>
             <a className="my-1 p-2 text-center text-orange-600 border border-orange-600 rounded-xl hover:text-black hover:bg-orange-600 shadow" href="https://www.youtube.com/channel/UCmCyGWHYIjGBdRdDigT2Erg" target="_blank">Chaîne Youtube</a>
         </nav>
     )
