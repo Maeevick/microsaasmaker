3c3603217b925dc0ca6f048fd5db88e17d5f30f1
Aurel <sayag_vs@hotmail.fr>
2022-11-29 21:37:16 +0100

    tech: subscription happy path

--- handlers/subscribe.test.ts
+++ handlers/subscribe.test.ts
@@ -1,7 +1,19 @@
-import { subscribeCommandHandler } from './subscribe'
+import { subscribeCommandHandler, SubscriberData } from './subscribe'
 
-describe('setup tests', () => {
-    test('is ok', () => {
-        expect(1).toEqual(2);
+describe('Subscription to Newsletter', () => {
+    test(`when the user fills firstname and email then they are saved as name and email`, async () => {
+        const fakeSubscribersPersistence: SubscriberData[] = []
+
+        const fakeSubscriber = { firstname: "Maeevick", email: "some_email@domain.ext" }
+        const fakeSubscriberGateway = {
+            gateway: {
+                save: async (data: SubscriberData) => {
+                    fakeSubscribersPersistence.push(data)
+                }
+            }
+        }
+        await subscribeCommandHandler(fakeSubscriberGateway)(fakeSubscriber)
+
+        expect(fakeSubscribersPersistence).toEqual([{ name: "Maeevick", email: "some_email@domain.ext" }]);
     });
 })
