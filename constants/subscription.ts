983435ad5cb7b06ad06f389fdedc6f24060d68fc
Aurel <sayag_vs@hotmail.fr>
2022-12-02 21:06:14 +0100

    feat: unsubscribe

--- constants/subscription.ts
+++ constants/subscription.ts
@@ -4,7 +4,7 @@ export const EMAIL_IS_MISSING = `email manquant !`
 export const ALREADY_SUBSCRIBED = `tu es déjà membre, à bientôt dans ta boîte mail !`
 export const NEWLY_SUBSCRIBED = `tout est ok, à bientôt dans ta boîte mail !`
 
-export const UNSUBSCRIBED = `en espérant te revoir !`
+export const UNSUBSCRIBED = `à bientôt qui sait ;)`
 export const NOT_SUBSCRIBED = `oops, je ne te trouve pas tu n'était déjà plus là`
 
 export type SubscriptionKoMessage = typeof FIRSTNAME_IS_MISSING | typeof EMAIL_IS_MISSING | typeof NOT_SUBSCRIBED
---
+++ pages/unsubscribe.tsx
@@ -0,0 +1,46 @@
+import { NextPage } from "next"
+import { useRouter } from 'next/router'
+import React from "react"
+
+type UnsubscribeTarget = {
+    email: { value: string },
+}
+
+const Unsubscribe: NextPage = () => {
+    const router = useRouter()
+    const [apiDynamicMessage, setApiDynamicMessage] = React.useState('toutes tes données seront supprimées !')
+    const handleSubmit = async (event: React.SyntheticEvent) => {
+        event.preventDefault()
+
+        const target = event.target as typeof event.target & UnsubscribeTarget
+
+        const response = await fetch('/api/unsubscribe', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email: target.email.value,
+            }),
+        })
+        const { status, message } = await response.json()
+
+        setApiDynamicMessage(message)
+        if (status === 'ok') setTimeout(() => router.push('/'), 2000)
+    }
+
+    return (
+        <>
+            <h1 className="m-6 text-xl md:text-4xl">Aie ! Si tu es là c'est que j'ai raté quelque chose...</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="flex flex-wrap justify-around m-3">
+                    <input required placeholder="ton email" className="m-3 p-2 border border-amber-300 shadow rounded-xl" type="text" id="email" name="email" pattern="^.+@.+\.\w+$" />
+                    <button className="m-3 p-2 bg-amber-300 border border-amber-300 hover:bg-orange-600 shadow rounded-xl font-bold text-center outline-none focus:outline-none" type="submit">Valider</button>
+                </div>
+            </form>
+            <div className="flex-col justify-center p-2 border-t border-solid border-slate-200 rounded-t">
+                <p className="italic text-orange-600">{apiDynamicMessage}</p>
+            </div>
+        </>
+    )
+}
+
+export default Unsubscribe
